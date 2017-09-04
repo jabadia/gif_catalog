2070def32cc23b993d0c16cbbaf83a1842b3a910
Javier Abadía <javi.abadia@gmail.com>
2017-09-04 20:06:29 +0200

    modelo de imágenes Gif

--- backend/models.py
+++ backend/models.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class GifPicture(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    gifurl = models.URLField()
+    upload_date = models.DateTimeField(auto_now_add=True)
