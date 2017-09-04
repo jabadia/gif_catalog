7e88b058975353c20c60d4fea0d745a155be527f
Javier Abadía <javi.abadia@gmail.com>
2017-09-04 19:51:24 +0200

    la primera vista de index en Django

--- gif_catalog/settings.py
+++ gif_catalog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend',
 ]
 
 MIDDLEWARE = [
@@ -118,5 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
