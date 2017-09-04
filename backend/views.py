62edfc3edc41793542d683edbc5a93e467a455f0
Javier Abadía <javi.abadia@gmail.com>
2017-09-04 19:51:24 +0200

    la primera vista de index en Django

--- backend/views.py
+++ backend/views.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'msg': "hi world!, I'm Django",
+    }
+    return render(request, 'index.html', context)
