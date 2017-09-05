a1439cb7ff59ce67adde761e64650133decda80a
Javier Abadía <javi.abadia@gmail.com>
2017-09-05 15:49:54 +0200

    GifHome component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,28 @@
 <template>
-    <div id="app">
-        <h1>Gif Catalog</h1>
-        {{msg}}
+    <div id="app" class="container">
+        <div class="row mb-4">
+            <div class="col-12">
+                <h1>Gif Catalog</h1>
+                {{msg}}
+            </div>
+        </div>
+        <gif-home></gif-home>
     </div>
 </template>
 
 <script>
+    import GifHome from './components/GifHome.vue';
+
     export default {
         name: 'app',
         data() {
             return {
                 msg: 'Welcome to Your Vue.js App!'
             }
-        }
+        },
+        components: {
+            GifHome,
+        },
     }
 </script>
 
---
+++ frontend/src/components/GifHome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            <ul>
+            <li v-for="pic in pics">{{pic.id}}.- {{pic.title}}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                pics: [],
+            };
+        },
+    };
+</script>
+
+<style lang="less" rel="stylesheet/less">
+</style>
