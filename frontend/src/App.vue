12afb3aacecf676d61a890d7a74689f3d691ac1c
Javier Abadía <javi.abadia@gmail.com>
2017-09-05 16:39:44 +0200

    logout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,12 @@
 <template>
     <div id="app" class="container">
         <div class="row mb-4">
-            <div class="col-12">
+            <div class="col">
                 <h1>Gif Catalog</h1>
                 {{msg}}
+            </div>
+            <div class="col-auto">
+                <a class="pull-right" href="/logout/">logout</a>
             </div>
         </div>
         <gif-home></gif-home>
