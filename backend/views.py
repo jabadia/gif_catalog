19b7e687cc6ca5c09fed935646475928dd3a40ca
Javier Abadía <javi.abadia@gmail.com>
2017-09-05 09:55:01 +0200

    implementación clásica de una vista de Django

--- backend/views.py
+++ backend/views.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import random
+
 from django.shortcuts import render
+
+from backend.models import GifPicture
 
 
 def index(request):
+    count = GifPicture.objects.all().count()
+    all_ids = range(count)
+    random.shuffle(all_ids)
+    picked_ids = all_ids[:18]
+
+    # gif_pictures = GifPicture.objects.all().order_by('-upload_date')[:18]
+    gif_pictures = GifPicture.objects.filter(id__in=picked_ids).order_by('-upload_date')
+
     context = {
-        'msg': "hi world!, I'm Django",
+        'msg': "randomly picked images",
+        'gif_pictures': gif_pictures,
     }
     return render(request, 'index.html', context)
