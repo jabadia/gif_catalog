66b5c4fc175f49e3fecc41340b0d987e1e2d7c59
Javier Abadía <javi.abadia@gmail.com>
2017-09-05 17:00:09 +0200

    decorador para proteger las llamadas a la API

--- backend/api.py
+++ backend/api.py
@@ -4,10 +4,17 @@ from django.http import JsonResponse
 from gif_picture_manager import GifPictureManager
 
 
-def pics(request):
-    if not request.user.is_authenticated:
-        return JsonResponse({'msg': 'unauthorized'}, status=403)
+def api_login_required(view_func):
+    def _wrapped_view_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return view_func(request, *args, **kwargs)
+        else:
+            return JsonResponse({'msg': 'unauthorized'}, status=403)
+    return _wrapped_view_func
 
+
+@api_login_required
+def pics(request):
     how_many = 18
     gif_pictures = [model_to_dict(pic) for pic in GifPictureManager.get_random_pics(how_many)]
 
