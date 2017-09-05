3832ec604f33ca621dfc38092862b1991215ff41
Javier Abadía <javi.abadia@gmail.com>
2017-09-05 17:51:42 +0200

    mejor serializacion

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,3 @@
-from django.forms import model_to_dict
 from django.http import JsonResponse
 
 from gif_picture_manager import GifPictureManager
@@ -13,10 +12,23 @@ def api_login_required(view_func):
     return _wrapped_view_func
 
 
+def _serialize_model(model, fields=None, exclude_fields=None, add_properties=None):
+    exclude_fields = exclude_fields or []
+    the_dict = {
+        field.name: field.value_from_object(model)
+        for field in model._meta.fields
+        if (fields is None or field.name in fields) and field.name not in exclude_fields
+    }
+    if add_properties:
+        for p in add_properties:
+            the_dict[p] = getattr(model, p)
+    return the_dict
+
+
 @api_login_required
 def pics(request):
     how_many = 18
-    gif_pictures = [model_to_dict(pic) for pic in GifPictureManager.get_random_pics(how_many)]
+    gif_pictures = [_serialize_model(pic) for pic in GifPictureManager.get_random_pics(how_many)]
 
     result = {
         'pics': gif_pictures,
