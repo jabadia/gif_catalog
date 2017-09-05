3fb9625180ddac312e956f179d6a6d621d03204a
Javier Abadía <javi.abadia@gmail.com>
2017-09-05 16:39:53 +0200

    esto ya no hacía falta!

--- backend/views.py
+++ backend/views.py
@@ -4,16 +4,10 @@ from __future__ import unicode_literals
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from gif_picture_manager import GifPictureManager
-
 
 @login_required
 def index(request):
-    how_many = 18
-    gif_pictures = GifPictureManager.get_random_pics(how_many)
-
     context = {
         'msg': "randomly picked images",
-        'gif_pictures': gif_pictures,
     }
     return render(request, 'index.html', context)
