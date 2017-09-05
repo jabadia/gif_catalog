940bc3c2e7c614b5690256dd98ad28de2ee303c8
Javier Abadía <javi.abadia@gmail.com>
2017-09-05 16:50:38 +0200

    proteger la api con autenticación

--- backend/api.py
+++ backend/api.py
@@ -5,6 +5,9 @@ from gif_picture_manager import GifPictureManager
 
 
 def pics(request):
+    if not request.user.is_authenticated:
+        return JsonResponse({'msg': 'unauthorized'}, status=403)
+
     how_many = 18
     gif_pictures = [model_to_dict(pic) for pic in GifPictureManager.get_random_pics(how_many)]
 
